d3be3ae5cabdff71db4eb94a59dc258d981c8a59
OgrodowskiJedrzej <jedrekogro@gmail.com>
2024-04-07 23:51:18 +0200

    Fixes

--- main.sh
+++ main.sh
@@ -1,47 +1,5 @@
 #!/bin/bash
 
-check_same_content() {
-    local file1="$1"
-    local file2="$2"
-    local mode="$3"
-    if [ "$file1" != "$file2" ]; then
-        if cmp -s "$file1" "$file2"; then
-            echo "Files $file1 and $file2 have same content."
-               if [ "$mode" = "-f" ]; then
-                    delete_duplicate_without_asking $file1
-               fi
-               if [ "$mode" = "-i" ]; then
-                    delete_duplicate_with_asking $file1
-               fi
-        fi
-    fi
-}
-
-delete_duplicate_without_asking() {
-    local file="$1"
-    rm -f "$file"
-}
-
-delete_duplicate_with_asking() {
-    local file="$1"
-    rm -i "$file"
-}
-
-
-compare_files() {
-    directory="$1"
-    mode="$2"
-
-    # Use find to get a list of all files (excluding directories) recursively
-    find "$directory" -type f -print0 | while IFS= read -r -d '' file1; do
-        while IFS= read -r -d '' file2; do
-            # Compare files if they have different names
-               check_same_content $file1 $file2 $mode
-        done < <(find "$directory" -type f -print0)
-    done
-}
-
-
 print_file_info() {
     local file="$1"
     
@@ -57,16 +15,54 @@ print_file_info() {
     fi
 }
 
+check_same_content() {
+    local file1="$1"
+    local file2="$2"
+    local mode="$3"
+    if [ "$file1" != "$file2" ]; then
+        if cmp -s "$file1" "$file2"; then
+            echo "Files $file1 and $file2 have same content."
+            print_file_info $file1
+            print_file_info $file2
+               if [ "$mode" = "-f" ]; then
+                    rm -f "$file1"
+               fi
+               if [ "$mode" = "-i" ]; then
+                    rm -i "$file1"
+               fi
+        fi
+    fi
+}
+
+compare_files() {
+    directory="$1"
+    mode="$2"
+    find "$directory" -type f -print0 | while IFS= read -r -d '' file1; do
+        while IFS= read -r -d '' file2; do
+               check_same_content $file1 $file2 $mode
+        done < <(find "$directory" -type f -print0)
+    done
+}
+
 print_help_info() {
     # to be added
-    echo "test"
+    echo "Usage: ./main.sh Parameter1 Parameter2"
+    echo "Parameter1 -f to delete file instantly"
+    echo "Parameter1 -i to ask before deleting"
+    echo "Parameter2 -n to delete file with same name"
+    echo "Parameter2 -c to delete file with same content"
+    exit 0
 }
 
 # main 
 path_to_directory="/Users/jendras/Prywatne/Books/"
+if [ -z "$1" ]; then
+    echo "Error: One or more parameters are empty"
+    print_help_info
+    exit 1 
+fi
 if [ "$1" = "-h" ]; then
     print_help_info
-    exit 0
 fi
 if [ "$1" = "-f" ]; then
     compare_files "$path_to_directory" "$1"
